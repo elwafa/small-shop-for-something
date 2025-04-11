2732321a82522a3f485c49fcda2720c0228e20e6
elwafa <eslamaboelwafa@gmail.com>
2025-04-11 17:05:58 +0200

    pointer

--- internal/entities/item.go
+++ internal/entities/item.go
@@ -1,22 +1,22 @@
 package entities
 
 type Item struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name" validate:"required"`
-	Price       float64 `json:"price" validate:"required"`
-	Picture     string  `json:"picture" validate:"required"`
-	Description string  `json:"description" validate:"required"`
-	Status      string  `json:"status" validate:"required,oneof=New Used Sold"`
-	UserId      int     `json:"user_id" validate:"required,numeric"`
-	Receive     string  `json:"receive" validate:"required,oneof=PickUp Deliver BothOptionsArePossible"`
-	Color       string  `json:"colour" validate:"required"`
-	Category    string  `json:"category" validate:"required"`
+	ID          int      `json:"id"`
+	Name        string   `json:"name" validate:"required"`
+	Price       *float64 `json:"price" validate:"required"`
+	Picture     string   `json:"picture" validate:"required"`
+	Description string   `json:"description" validate:"required"`
+	Status      string   `json:"status" validate:"required,oneof=New Used Sold"`
+	UserId      int      `json:"user_id" validate:"required,numeric"`
+	Receive     string   `json:"receive" validate:"required,oneof=PickUp Deliver BothOptionsArePossible"`
+	Color       string   `json:"colour" validate:"required"`
+	Category    string   `json:"category" validate:"required"`
 }
 
 func NewItem(name, picture, description, status, receive, color, category string, price float64, userId int) (*Item, error) {
 	item := &Item{
 		Name:        name,
-		Price:       price,
+		Price:       &price,
 		Picture:     picture,
 		Description: description,
 		Status:      status,
